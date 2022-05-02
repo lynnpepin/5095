bc82317aea54b1b57cbde4dab96d1424d1a05d5e
lynnpepin <lynnpepin@tutanota.com>
2022-05-02 00:13:34 -0400

    Barebones main for engine rendering

--- main.py
+++ main.py
@@ -10,10 +10,37 @@ import sys
 import matplotlib
 import numpy as np
 
+import pygame
+
+def main(width: int = 360, height: int = 360):
+    # See https://dr0id.bitbucket.io/legacy/pygame_tutorial00.html
+    pygame.init()
+    pygame.display.set_caption("5095 go vroom")
+    screen = pygame.display.set_mode((width,height))
+
+    running = True
+    while running:
+        for ev in pygame.event.get():
+            if ev.type == pygame.QUIT:
+                running = False
+
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="")
-    parser.add_argument("--number","-n", default=[0],
-                        help="Integer argument.",
-                        type=int, nargs=1)
+    parser.add_argument(
+        "--width","-scrw", 
+        default=[360],
+        help="Integer argument.",
+        type=int, nargs=1
+    )
+    parser.add_argument(
+        "--height","-scrh", 
+        default=[360],
+        help="Integer argument.",
+        type=int, nargs=1
+    )
     args = parser.parse_args()
-    print(args)
+    print(args.width[0], args.height[0])
+
+    main(args.width[0], args.height[0])
